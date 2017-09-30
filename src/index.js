1bbb3c7ed12ada640b26e5964a2ef002cb657720
John Kagga <johnkagga@gmail.com>
2017-09-30 08:31:24 +0300

    Added font awesome icons to the homepage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css'
 import './css/index.css'
 import './components/Application'
 import Application from './components/Application'
