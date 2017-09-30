6e52c354d3322f7042c41b03960b0462cefa7b8d
John Kagga <johnkagga@gmail.com>
2017-09-30 08:16:13 +0300

    Added navlink to navbar items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () =>
   <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-    <a className="navbar-brand" href="#">BucketList</a>
+    <NavLink className="navbar-brand" exact to="/">BucketList</NavLink>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -10,14 +11,14 @@ const Navbar = () =>
 
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ml-auto">
-        <li className="nav-item active">
-          <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <li className="nav-item">
+          <NavLink className="nav-link" exact to="/">Home</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Login</a>
+          <NavLink className="nav-link" to="/login">Login</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Sign Up</a>
+          <NavLink className="nav-link" to="/signup">Sign Up</NavLink>
         </li>
       </ul>
     </div>
