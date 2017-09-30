42fd100198f2fc7c8122b390883753e8edec19e7
John Kagga <johnkagga@gmail.com>
2017-09-30 08:43:01 +0300

    Added a link to the get started home button

--- src/components/home/Jumbotron.js
+++ src/components/home/Jumbotron.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 const Jumbotron = () =>
   <div className="jumbotron">
     <h1 className="display-3">Dreams and Goals tracking app</h1>
     <p className="lead">
-      BucketList app allows you to record and share things you want to achieve or experience before reaching a
+      BucketList app allows you to record and share things you want to achieve or experience before reaching NavBar
       certain
       age.
     </p>
     <hr className="my-4"/>
     <p className="lead text-center">
-      <a className="btn btn-primary btn-lg text-center" href="#" role="button">Get Started</a>
+      <NavLink className="btn btn-primary btn-lg text-center" exact to="/signup" role="button">Get Started</NavLink>
     </p>
   </div>
 
