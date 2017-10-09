ef66a8ddb77ac6a719aa7d4170d1431c9ec7989a
johnkagga <johnkagga@gmail.com>
2017-10-09 15:08:33 +0300

    Fixed bug where after log out the email and logout in the navbar would persist

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,54 +5,54 @@ import {USER_EMAIL} from "../utilities/Constants";
 
 class Navbar extends React.Component {
 
-    render() {
-        const email = localStorage.getItem(USER_EMAIL);
-        return (
-            <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-                <NavLink className="navbar-brand" exact to="/">BucketList</NavLink>
-                <button className="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+  render() {
+    const email = localStorage.getItem(USER_EMAIL);
+    return (
+        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+          <NavLink className="navbar-brand" exact to="/">BucketList</NavLink>
+          <button className="navbar-toggler" type="button" data-toggle="collapse"
+                  data-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-                {email ?
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ml-auto">
-                            <li className="nav-item">
-                                <NavLink className="nav-link" exact to="/">Home</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink exact className="nav-link" to="/buckets">{email}</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink exact className="nav-link" to="/logout">logout</NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                    :
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ml-auto">
-                            <li className="nav-item">
-                                <NavLink className="nav-link" exact to="/">Home</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/login">Login</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/signup">Sign Up</NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                }
-            </nav>
-        )
-    }
+          {this.props.isAuthenticated ?
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <NavLink className="nav-link" exact to="/">Home</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink exact className="nav-link" to="/buckets">{email}</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink exact className="nav-link" to="/logout">logout</NavLink>
+                  </li>
+                </ul>
+              </div>
+              :
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <NavLink className="nav-link" exact to="/">Home</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/login">Login</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/signup">Sign Up</NavLink>
+                  </li>
+                </ul>
+              </div>
+          }
+        </nav>
+    )
+  }
 }
 
 Navbar.propTypes = {
-    isAuthenticated: PropTypes.bool.isRequired,
-    dispatch: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool.isRequired,
+  dispatch: PropTypes.func.isRequired,
 };
 
 
