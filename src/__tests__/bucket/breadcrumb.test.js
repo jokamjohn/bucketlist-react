05d5002d9c1f5924388fa8d92362ebf8bf8b5530
johnkagga <johnkagga@gmail.com>
2017-12-04 17:48:00 +0300

    Add Bucket BreadCrumbs tests

---
+++ src/__tests__/bucket/breadcrumb.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+
+Enzyme.configure({adapter: new Adapter()});
+
+import {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Breadcrumb from '../../components/bucket/Breadcrumb'
+
+
+const setUp = () => {
+  return shallow(<Breadcrumb/>);
+};
+
+describe('Test Bucket Breadcrumb', () => {
+  it('first li text is Home', () => {
+    const wrapper = setUp();
+    expect(wrapper.find('li').at(0).text()).toEqual('Home')
+  });
+
+  it('second li text is Buckets', () => {
+    const wrapper = setUp();
+    expect(wrapper.find('li').at(1).text()).toEqual('Buckets')
+  });
+
+  it('assert that the home href is / ', () => {
+    const wrapper = setUp();
+    expect(wrapper.find('a').at(0).props().href).toEqual('/')
+  });
+
+  it('assert that the buckets href is /buckets ', () => {
+    const wrapper = setUp();
+    expect(wrapper.find('a').at(1).props().href).toEqual('/buckets')
+  });
+});
